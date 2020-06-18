9255597c5f74cb5be6053c51a032c26842b2cd6d
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-18 15:10:16 -0400

    exercise 1.1

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,25 @@
 // Exercise 1.1
 // ------------
-console.log('exercise 1.1');
+
+const clickMe = document.querySelector("body");
+const game = document.querySelector("p");
+const rules = document.createElement("h2");
+let winner = false;
+let loser = false;
+
+rules.innerText = "Be a quicker clicker!";
+clickMe.prepend(rules);
+
+addEventListener("click", function () {
+  if (loser === false) {
+    game.innerText = "You Win";
+    winner = true;
+  }
+});
+
+setTimeout(function () {
+  if (winner === false) {
+    game.innerText = "You Lose";
+    loser = true;
+  }
+}, 1000);
