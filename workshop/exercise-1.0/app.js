36e758f1e2c8a553513ff6d443db63c9ec468dff
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-18 11:44:53 -0400

    exercise 1.1

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,8 @@
 // Exercise 1.0
 // ------------
-console.log('exercise-1');
+
+const body = document.querySelector("body");
+
+addEventListener("click", function () {
+  body.innerText = "You Win!";
+});
