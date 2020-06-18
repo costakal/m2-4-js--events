fe30c2d65258789180c2f49cb5725115774a7dd3
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-18 17:14:23 -0400

    exercise 1-2

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,25 @@
 // Exercise 1.2
 // ------------
-console.log('exercise 1.2');
+const game = document.querySelector("body");
+const result = document.querySelector("#result");
+const time = document.querySelector("#time");
+
+let winner = false;
+let loser = false;
+let randomNumber = Math.floor(Math.random() * 5000);
+let randomNumberView = Math.floor(randomNumber / 1000);
+time.innerText = randomNumberView;
+
+addEventListener("click", function () {
+  if (loser === false) {
+    result.innerText = "You Win";
+    winner = true;
+  }
+});
+
+setTimeout(function () {
+  if (winner === false) {
+    result.innerText = "You Lose";
+    loser = true;
+  }
+}, randomNumber);
